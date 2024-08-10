a7ab577d5b0b2cbf6767d54a9b1f70bc06aa3c47
longlo2k3 <168855062+longlo2k3@users.noreply.github.com>
2024-08-10 16:32:55 +0700

    test commit

--- ViewModel/ClubDetailsViewModel.cs
+++ ViewModel/ClubDetailsViewModel.cs
@@ -5,7 +5,6 @@ namespace Object.ViewModel
     public class ClubDetailsViewModel
     {
         public Club Club { get; set; }
-        ascascasc
         public IEnumerable<Club> ClubList { get; set; }
     }
 }
