5f12b86e19c6c28932626fc28bc70afef2b8b5fc
longlo2k3 <168855062+longlo2k3@users.noreply.github.com>
2024-08-10 16:32:36 +0700

    commit update

--- ViewModel/ClubDetailsViewModel.cs
+++ ViewModel/ClubDetailsViewModel.cs
@@ -5,6 +5,7 @@ namespace Object.ViewModel
     public class ClubDetailsViewModel
     {
         public Club Club { get; set; }
+        ascascasc
         public IEnumerable<Club> ClubList { get; set; }
     }
 }
